674e54e3158569d95014e6d0a4918a4e39d8e40f
RUNO <nmryuga10@gmail.com>
2026-06-15 20:53:36 +0900

    関数を呼び出した

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 function greet (){
   console.log("Hello from the remote branch");
 }
-=======
-function greet() {
-    console.log("Hello from the local branch");
-}
->>>>>>> e8ecc88 (挨拶関数を実装)
+
+greet
