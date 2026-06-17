f9ce1893979a881579887b309ba6a182d57a1fd6
RUNO <nmryuga10@gmail.com>
2026-06-17 15:10:45 +0900

    ai.pyの削除

--- ai.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="gpt2")
-
-result = generator("I love pizza bicause", max_length=50)
-
-print(result[0]["generated_text"])
